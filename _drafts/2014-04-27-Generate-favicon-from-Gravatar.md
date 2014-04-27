38e4aa4db0335734f4e945a51bff1a59dd0f784f
Benjamin Fradet <benjamin.fradet@gmail.com>
2014-04-27 19:05:30 +0200

    layouts

---
+++ _drafts/2014-04-27-Generate-favicon-from-Gravatar.md
@@ -0,0 +1,14 @@
+---
+layout: post
+title: Generate favicon from Gravatar
+categories:
+    - how-to
+    - trick
+tags:
+    - how-to
+    - trick
+---
+
+-yum install netpbm-progs
+-python script
+-bash script
---
+++ scripts/grvToFav.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+while getopts e:s: option;
+do
+    case "${option}" 
+    in
+        e) EMAIL=${OPTARG};;
+        s) SIZE=${OPTARG};;
+    esac
+done
+
+URL=$(python getHash.py -e $EMAIL -s $SIZE)
+wget $URL -O /tmp/favicon.png
+
+pngtopnm -mix /tmp/favicon.png > tmp_logo.pnm
+
+rm -f ../favicon.ico
+
+pnmscale -xsize=32 -ysize=32 ./tmp_logo.pnm > tmp_logo32.ppm
+pnmscale -xsize=16 -ysize=16 ./tmp_logo.pnm > tmp_logo16.ppm
+
+pnmquant 256 tmp_logo32.ppm > tmp_logo32x32.ppm
+pnmquant 256 tmp_logo16.ppm > tmp_logo16x16.ppm
+
+ppmtowinicon -output ../favicon.ico tmp_logo16x16.ppm tmp_logo32x32.ppm
+
+rm -f tmp_logo* /tmp/favicon.png
