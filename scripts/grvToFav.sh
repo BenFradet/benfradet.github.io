34fcd179347caabaef280e7a0b9e45de748c04da
Benjamin Fradet <benjamin.fradet@gmail.com>
2014-04-29 17:50:59 +0200

    script

--- scripts/grvToFav.sh
+++ scripts/grvToFav.sh
@@ -14,7 +14,7 @@ wget $URL -O /tmp/favicon.png
 
 pngtopnm -mix /tmp/favicon.png > tmp_logo.pnm
 
-rm -f ../favicon.ico ../_site/favicon.ico
+rm -f favicon.ico _site/favicon.ico
 
 pnmscale -xsize=32 -ysize=32 ./tmp_logo.pnm > tmp_logo32.ppm
 pnmscale -xsize=16 -ysize=16 ./tmp_logo.pnm > tmp_logo16.ppm
@@ -22,6 +22,8 @@ pnmscale -xsize=16 -ysize=16 ./tmp_logo.pnm > tmp_logo16.ppm
 pnmquant 256 tmp_logo32.ppm > tmp_logo32x32.ppm
 pnmquant 256 tmp_logo16.ppm > tmp_logo16x16.ppm
 
-ppmtowinicon -output ../favicon.ico tmp_logo16x16.ppm tmp_logo32x32.ppm
+ppmtowinicon -output favicon.ico tmp_logo16x16.ppm tmp_logo32x32.ppm
 
 rm -f tmp_logo* /tmp/favicon.png
+
+cp favicon.ico _site/favicon.ico
